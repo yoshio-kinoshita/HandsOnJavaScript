0732421c7adb16d141e4eb89ab430de68b901fcf
木下喜雄 <kinoshitayoshio@kinoshitayoshionoMacBook-Air.local>
2022-01-02 22:24:40 +0900

    ch4 update

--- ch4/objectClass.js
+++ ch4/objectClass.js
@@ -79,3 +79,109 @@ console.log(checkStatus(Object.preventExtensions({ answer: 42 })))
 console.log(checkStatus(Object.seal({ answer: 42 })))
 console.log(checkStatus(Object.freeze({ answer: 42 })))
 
+/**
+ * プロパティを一覧する
+ *  Object.getOwnPropertyNames();
+ *  Object.getOwnPropertySymbols();
+ *  Object.keys();
+ *  Object.values()
+ *  Object.entries()
+ */
+let obj = { bro1: "Mycroft", bro2: "Sherlock", [Symbol("sys")]: "Eurus" }
+
+console.log(Object.getOwnPropertyNames(obj))
+console.log(Object.getOwnPropertySymbols(obj))
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+const clone = Object.fromEntries(Object.entries(obj))
+console.log(clone)
+
+/**
+ * プロパティを管理する　
+ * Object.definProperty()
+ * Object.getOwnPropertyDescriptor()
+ * Object.getOwnPropertyDescriptors()
+ */
+
+let vet = {}
+
+Object.defineProperties(vet, {
+    x: { value: 3, writable: true, enumerable: true, configurable: true },
+    y: { value: 4, writable: true, enumerable: true }
+})
+
+console.log(vet)
+
+Object.defineProperty(vet, "length", {
+    enumerable: false,
+    get: () => {
+        return Math.sqrt(this.x * this * x + this.y * this.y)
+    },
+    set: (len) => {
+        const current = this.length
+        this.x *= len / current
+        this.y *= len / current
+    }
+})
+
+console.log(vet)
+
+console.log(Object.getOwnPropertyDescriptors(vet))
+console.log(Object.getOwnPropertyDescriptor(vet, "length"))
+
+Object.defineProperty(vet, "x", { value: 5, enumerable: false })
+
+console.log(Object.keys(vet))
+console.log(Object.getOwnPropertyDescriptors(vet))
+console.log(vet.x)
+
+// TypeError: Cannot redefine property: y
+// Object.defineProperty(vet, "y", {value: 5, writable: true, enumerable: false})
+
+vet.z = 1
+
+console.log(Object.getOwnPropertyDescriptors(vet))
+
+// プロパティをコピーする
+const parameters = { power: "B", speed: "c" },
+    params1 = { range: "D" },
+    params2 = { durability: "C" }
+
+console.log(Object.assign(parameters, params1, params2))
+
+const clone2 = Object.assign({}, parameters)
+console.log(clone2)
+
+console.log(parameters == clone2)
+console.log(parameters === clone2)
+console.log(parameters != clone2)
+console.log(parameters !== clone2)
+console.log(Object.is(parameters, clone2))
+
+const param3 = {
+    [Symbol("percision")]: "D",
+    get development() {
+        return "C"
+    }
+}
+
+Object.assign(parameters, param3)
+console.log(parameters)
+
+/**
+ * オブジェクトのクラスを確認する
+ */
+
+class Vector2{}
+let vec = new Vector2()
+console.log(typeof vec);
+
+console.log(vec instanceof Vector2)
+console.log(vec instanceof Object)
+console.log(vec instanceof Date)
+
+console.log(Object.getPrototypeOf(vec))
+
+Object.getPrototypeOf(vec)
